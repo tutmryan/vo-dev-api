8d5492b6b8b30350304ccab28bf488a4ef0c3a95
Sam Curry <sam.curry@makerx.com.au>
2023-03-31 17:32:09 +0800

    add delete template

---
+++ src/features/templates/commands/delete-template-command.ts
@@ -0,0 +1,8 @@
+import type { CommandContext } from '../../../cqrs/command-context'
+import { TemplateEntity } from '../entities/template-entity'
+
+export async function DeleteTemplateCommand(this: CommandContext, id: string) {
+  const repo = this.entityManager.getRepository(TemplateEntity)
+  const template = await repo.findOneByOrFail({ id })
+  await repo.remove(template)
+}
--- src/features/templates/resolvers.ts
+++ src/features/templates/resolvers.ts
@@ -2,6 +2,7 @@ import { dispatch, query } from '../../cqrs/dispatcher'
 import type { Resolvers } from '../../generated/graphql'
 import { resolveUpdatedAt } from '../auditing/updated-at-resolver'
 import { CreateTemplateCommand } from './commands/create-template-command'
+import { DeleteTemplateCommand } from './commands/delete-template-command'
 import { UpdateTemplateCommand } from './commands/update-template-command'
 import { FindTemplatesQuery } from './queries/find-templates-query'
 import { GetTemplateQuery } from './queries/get-template-query'
@@ -10,6 +11,7 @@ export const resolvers: Resolvers = {
   Mutation: {
     createTemplate: (_, { input }, context) => dispatch(context, CreateTemplateCommand, input),
     updateTemplate: (_, { id, input }, context) => dispatch(context, UpdateTemplateCommand, id, input),
+    deleteTemplate: (_, { id }, context) => dispatch(context, DeleteTemplateCommand, id),
   },
   Query: {
     template: (_, { id }, context) => query(context, GetTemplateQuery, id),
