95d35c9369acf92b2734d158e40ef1a3f1f865c6
Sam Curry <sam.curry@makerx.com.au>
2023-04-11 13:47:08 +0800

    update scalars schema after updating scalars package, remove unused cacheControl and constraint schema def

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,10 @@
 import { loadFilesSync } from '@graphql-tools/load-files'
-import { mergeResolvers } from '@graphql-tools/merge'
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { applyMiddleware } from 'graphql-middleware'
+import { resolvers as scalarResolvers, typeDefs as scalarTypeDefs } from 'graphql-scalars'
 import path from 'path'
 import { permissions } from './shield'
-import { pick } from 'lodash'
-import { resolvers as scalarResolvers } from 'graphql-scalars'
-import { identityFunc } from 'graphql/jsutils/identityFunc'
-import type { GraphQLSchema } from 'graphql'
-import { GraphQLScalarType } from 'graphql'
 
 export default function () {
   const resolvers = loadFilesSync(path.join(__dirname, './features/**/resolvers.*'), { extensions: ['ts', 'js'] })
@@ -17,43 +13,12 @@ export default function () {
     path.join(__dirname, './schema/**/*.graphql'),
   ])
 
-  const usedScalars = pick(scalarResolvers, 'String', 'PositiveInt', 'URL', 'HexColorCode', 'Void', 'Locale', 'DateTime')
-
   let schema = makeExecutableSchema({
-    typeDefs,
-    resolvers: mergeResolvers([usedScalars, ...resolvers]),
+    typeDefs: mergeTypeDefs([scalarTypeDefs, ...typeDefs]),
+    resolvers: mergeResolvers([scalarResolvers, ...resolvers]),
   })
-
-  requireExplicitResolversForScalars(schema)
-
-  const scalarsWithNoExplicitResolvers = Object.values(schema.getTypeMap())
-    .filter((x) => x instanceof GraphQLScalarType)
-    .map((x) => x as GraphQLScalarType)
-    .filter((x) => x.parseValue === identityFunc && x.serialize === identityFunc)
-    .map((x) => x.name)
-
-  if (scalarsWithNoExplicitResolvers.length > 0) {
-    throw new Error(`The following scalars have no explicit resolvers: ${scalarsWithNoExplicitResolvers.join(', ')}`)
-  }
 
   schema = applyMiddleware(schema, permissions)
 
   return schema
-}
-
-function requireExplicitResolversForScalars(schema: GraphQLSchema) {
-  const testSymbol = Symbol('testSymbol')
-  const scalars = Object.values(schema.getTypeMap()).filter((t): t is GraphQLScalarType => t instanceof GraphQLScalarType)
-  for (const scalar of scalars) {
-    if (scalar.name === 'Void') continue
-    try {
-      scalar.parseValue(testSymbol)
-    } catch {
-      // We expect that a parser should not handle parsing a symbol
-      continue
-    }
-    throw new Error(
-      `Scalar ${scalar.name} does not appear to have an explicit resolver, or is not performing sufficient validation when parsing input`,
-    )
-  }
 }
