461dfe4a1a14634468794ddf8baf4969c3bad0ed
Sam Curry <sam.curry@makerx.com.au>
2023-04-11 13:37:17 +0800

    upgrade packages, use typed-config-factory

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
-import { typedConfig } from './typed-config'
-
-export default typedConfig
+import { createTypedConfig } from '@makerxstudio/node-common/typed-config-factory'
+import type { Config } from './schema'
+// eslint-disable-next-line no-restricted-imports
+import config from 'config'
+export default createTypedConfig<Config>(config)
