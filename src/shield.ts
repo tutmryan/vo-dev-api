3c4fe7045ea732014244cd993dae5c84c7702949
Sam Curry <sam.curry@makerx.com.au>
2023-04-12 14:27:49 +0800

    fix fallback auth rule

--- src/shield.ts
+++ src/shield.ts
@@ -1,5 +1,5 @@
 import { isDev, isLocalDev } from '@makerxstudio/node-common'
-import { allow, race, rule, shield } from 'graphql-shield'
+import { allow, chain, rule, shield } from 'graphql-shield'
 import type { IRules, ShieldRule } from 'graphql-shield/typings/types'
 import type { GraphQLContext } from './context'
 import type { Resolvers } from './generated/graphql'
@@ -7,7 +7,7 @@ import type { Resolvers } from './generated/graphql'
 const isAdmin = rule({ cache: 'contextual' })(async (_parent, _args, { user }: GraphQLContext) => user?.scopes.includes('Admin') === true)
 const canRequestIssuance = rule({ cache: 'contextual' })((_, __, { user }) => user?.roles.includes('VerifiableCredential.Issue'))
 const canRequestPresentation = rule({ cache: 'contextual' })((_, __, { user }) => user?.roles.includes('VerifiableCredential.Present'))
-const isUser = race(isAdmin, canRequestIssuance, canRequestPresentation)
+const isAuthorized = chain(isAdmin, canRequestIssuance, canRequestPresentation)
 
 export const permissions = wrappedShield({
   Query: {
@@ -17,7 +17,7 @@ export const permissions = wrappedShield({
 
 function wrappedShield(x: ShieldSchema<Resolvers>) {
   return shield(x as IRules, {
-    fallbackRule: isUser,
+    fallbackRule: isAuthorized,
     debug: isLocalDev || isDev, // [doc](https://the-guild.dev/graphql/shield/docs/shield) says: _Toggle debug mode._ (???)
     allowExternalErrors: true, // we don't want shield to catch and convert all errors to: Not Authorised!
   })
