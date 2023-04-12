a2499ac620c7a1d743044ad89207b97a77528420
Sam Curry <sam.curry@makerx.com.au>
2023-04-12 14:44:47 +0800

    Permit issuance and presentation consumers to read contracts

--- src/shield.ts
+++ src/shield.ts
@@ -1,5 +1,5 @@
 import { isDev, isLocalDev } from '@makerxstudio/node-common'
-import { allow, chain, rule, shield } from 'graphql-shield'
+import { allow, or, rule, shield } from 'graphql-shield'
 import type { IRules, ShieldRule } from 'graphql-shield/typings/types'
 import type { GraphQLContext } from './context'
 import type { Resolvers } from './generated/graphql'
@@ -7,17 +7,19 @@ import type { Resolvers } from './generated/graphql'
 const isAdmin = rule({ cache: 'contextual' })(async (_parent, _args, { user }: GraphQLContext) => user?.scopes.includes('Admin') === true)
 const canRequestIssuance = rule({ cache: 'contextual' })((_, __, { user }) => user?.roles.includes('VerifiableCredential.Issue'))
 const canRequestPresentation = rule({ cache: 'contextual' })((_, __, { user }) => user?.roles.includes('VerifiableCredential.Present'))
-const isAuthorized = chain(isAdmin, canRequestIssuance, canRequestPresentation)
+const isAuthorised = or(isAdmin, canRequestIssuance, canRequestPresentation)
 
 export const permissions = wrappedShield({
   Query: {
     healthcheck: allow,
+    contract: isAuthorised,
+    findContracts: isAuthorised,
   },
 })
 
 function wrappedShield(x: ShieldSchema<Resolvers>) {
   return shield(x as IRules, {
-    fallbackRule: isAuthorized,
+    fallbackRule: isAdmin,
     debug: isLocalDev || isDev, // [doc](https://the-guild.dev/graphql/shield/docs/shield) says: _Toggle debug mode._ (???)
     allowExternalErrors: true, // we don't want shield to catch and convert all errors to: Not Authorised!
   })
