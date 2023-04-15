4b0992d17d9e64e20759fcb355b96ba38ded82e0
Sam Curry <sam.curry@makerx.com.au>
2023-04-15 15:08:48 +0800

    prevent cache logging when under test

--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,7 @@
 import { KeyvAdapter } from '@apollo/utils.keyvadapter'
 import type { KeyValueCache } from '@apollo/utils.keyvaluecache'
 import { InMemoryLRUCache, PrefixingKeyValueCache } from '@apollo/utils.keyvaluecache'
-import { isLocalDev } from '@makerxstudio/node-common'
+import { environment, isLocalDev } from '@makerxstudio/node-common'
 import Keyv from 'keyv'
 import config from './config'
 import type { RequestCredential } from './generated/graphql'
@@ -10,8 +10,9 @@ import { logger } from './logger'
 const redis = config.has('redis') ? config.get('redis') : undefined
 const isRedisEnabled = !!redis?.host
 
-if (!isLocalDev && !isRedisEnabled) logger.warn('Redis caching is not configured, falling back to in-memory cache')
-else logger.info(`Caching configured using ${isRedisEnabled ? 'Redis' : 'in-memory'} cache`)
+if (!isLocalDev && !isRedisEnabled) {
+  if (environment !== 'test') logger.warn('Redis caching is not configured, falling back to in-memory cache')
+} else logger.info(`Caching configured using ${isRedisEnabled ? 'Redis' : 'in-memory'} cache`)
 
 const cache: KeyValueCache = isRedisEnabled
   ? new KeyvAdapter(new Keyv(`rediss://:${redis.key}@${redis.host}:6380`))
