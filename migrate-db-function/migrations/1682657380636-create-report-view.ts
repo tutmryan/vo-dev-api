e1ee67d8bb83afb9e24630e9d8343a6e7e26e080
inaie ignacio <inaie.ignacio@makerx.com.au>
2023-04-28 14:22:42 +0800

    rename view

--- migrate-db-function/migrations/1682657380636-create-report-view.ts
+++ migrate-db-function/migrations/1682657380636-create-report-view.ts
@@ -3,22 +3,22 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class CreateReportView1682657380636 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        CREATE VIEW report_view_credentials AS
+        CREATE VIEW credentials_view AS
         SELECT * FROM contract
       `)
 
     await queryRunner.query(`
-        GRANT SELECT ON report_view_credentials TO report_data_viewer
+        GRANT SELECT ON credentials_view TO report_data_viewer
       `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      REVOKE SELECT ON report_view_credentials FROM report_data_viewer
+      REVOKE SELECT ON credentials_view FROM report_data_viewer
       `)
 
     await queryRunner.query(`
-        DROP VIEW report_view_credentials
+        DROP VIEW credentials_view
       `)
   }
 }
