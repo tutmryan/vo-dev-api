148c00b980fbb56e98aae4f9b771b6b2389d51da
Sam Curry <sam.curry@makerx.com.au>
2023-05-10 11:26:20 +0800

    add local dev guard around adding api_user to report_data_viewer role

--- migrate-db-function/migrations/1683615537059-add-api_user-to-report_data_viewer.ts
+++ migrate-db-function/migrations/1683615537059-add-api_user-to-report_data_viewer.ts
@@ -2,14 +2,11 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class AddApiUserToReportDataViewer1683615537059 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-        IF EXISTS
-          (SELECT name
-          FROM master.sys.server_principals
-          WHERE name = 'api_user')
-        BEGIN
-          ALTER ROLE [report_data_viewer] ADD MEMBER [api_user]
-        END
+    const isLocalDevMigration = process.env.DATABASE_USERNAME === 'sa'
+
+    if (isLocalDevMigration)
+      await queryRunner.query(`
+        ALTER ROLE [report_data_viewer] ADD MEMBER [api_user]
       `)
   }
 
