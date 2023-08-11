70ce150f950859317c0ebc32f3cd784351c3bb20
Bobby Lat <bobby.lat@makerx.com.au>
2023-08-11 15:58:54 +0800

    cache presented credentials from callback event and include them in query result within TTL

---
+++ src/features/presentation/presented-credentials-resolver.test.ts
@@ -0,0 +1,86 @@
+import { PRESENTED_CREDENTIALS_TTL } from '../../cache'
+import type { User } from '../../user'
+import type { PresentationEntity } from './entities/presentation-entity'
+import { resolvePresentedCredentials } from './presented-credentials-resolver'
+
+const mockPresentedCredentialsCacheGet = jest.fn()
+jest.mock('../../cache', () => {
+  const originalModule = jest.requireActual('../../cache')
+  return {
+    ...originalModule,
+    get presentedCredentialsCache() {
+      return {
+        get: mockPresentedCredentialsCacheGet,
+      }
+    },
+  }
+})
+
+describe('presentedCredentialsResolver', () => {
+  const currentUserId = 'user-1'
+  it('does not load from cache if TTL has expired', () => {
+    // Arrange
+    const presentedAt = new Date(Date.now() - (PRESENTED_CREDENTIALS_TTL + 10) * 1000)
+    const entity = givenAPresentationEntity({ presentedAt: presentedAt, userId: currentUserId })
+    const currentUser = givenAUser(currentUserId)
+
+    // Act
+    resolvePresentedCredentials(entity, currentUser)
+
+    // Assert
+    expect(mockPresentedCredentialsCacheGet).not.toHaveBeenCalled()
+  })
+
+  it('does not load from cache if TTL has just reached', () => {
+    // Arrange
+    const presentedAt = new Date(Date.now() - PRESENTED_CREDENTIALS_TTL * 1000)
+    const entity = givenAPresentationEntity({ presentedAt: presentedAt, userId: currentUserId })
+    const currentUser = givenAUser(currentUserId)
+
+    // Act
+    resolvePresentedCredentials(entity, currentUser)
+
+    // Assert
+    expect(mockPresentedCredentialsCacheGet).not.toHaveBeenCalled()
+  })
+
+  it('does not load from cache if current user did not make the presentation request', () => {
+    // Arrange
+    const presentedAt = new Date(Date.now() - (PRESENTED_CREDENTIALS_TTL - 10) * 1000)
+    const entity = givenAPresentationEntity({ presentedAt: presentedAt, userId: 'user-2' })
+    const currentUser = givenAUser(currentUserId)
+
+    // Act
+    resolvePresentedCredentials(entity, currentUser)
+
+    // Assert
+    expect(mockPresentedCredentialsCacheGet).not.toHaveBeenCalled()
+  })
+
+  it('load from cache if TTL has not yet expired', () => {
+    // Arrange
+    const presentedAt = new Date(Date.now() - (PRESENTED_CREDENTIALS_TTL - 10) * 1000)
+    const entity = givenAPresentationEntity({ presentedAt: presentedAt, userId: currentUserId })
+    const currentUser = givenAUser(currentUserId)
+
+    // Act
+    resolvePresentedCredentials(entity, currentUser)
+
+    // Assert
+    expect(mockPresentedCredentialsCacheGet).toHaveBeenCalled()
+  })
+})
+
+const givenAPresentationEntity = (args: Partial<PresentationEntity>) => {
+  return {
+    requestId: 'request-1',
+    identityId: 'identity-1',
+    userId: 'user-1',
+    ...args,
+    issuanceIds: args.issuanceIds || ['issuance-1', 'issuance-2'],
+    requestedCredentials: args.requestedCredentials || [],
+    presentedCredentials: args.presentedCredentials || [],
+  } as PresentationEntity
+}
+
+const givenAUser = (userId: string): User => ({ userEntity: { id: userId } } as User)
---
+++ src/features/presentation/presented-credentials-resolver.ts
@@ -0,0 +1,27 @@
+import { PRESENTED_CREDENTIALS_TTL, presentedCredentialsCache } from '../../cache'
+import type { PresentedCredential } from '../../generated/graphql'
+import type { User } from '../../user'
+import type { PresentationEntity } from './entities/presentation-entity'
+
+export const resolvePresentedCredentials = async (
+  presentationEntity: PresentationEntity,
+  user: User | undefined,
+): Promise<PresentedCredential[]> => {
+  const entityValue = presentationEntity.presentedCredentials.map((c) => ({ ...c, claims: {} }))
+  if (
+    hasPresentedCredentialsTTLExpired(presentationEntity) ||
+    !didCurrentUserRequestPresentation(presentationEntity, user) ||
+    !presentationEntity.requestId
+  )
+    return entityValue
+
+  const presentedCredentialsDetails = await presentedCredentialsCache.get(presentationEntity.requestId)
+  if (!presentedCredentialsDetails) return entityValue
+  return JSON.parse(presentedCredentialsDetails) as PresentedCredential[]
+}
+
+const hasPresentedCredentialsTTLExpired = (presentationEntity: PresentationEntity) =>
+  presentationEntity.presentedAt.getTime() + PRESENTED_CREDENTIALS_TTL * 1000 <= Date.now()
+
+const didCurrentUserRequestPresentation = (presentationEntity: PresentationEntity, user: User | undefined) =>
+  user && user.userEntity.id === presentationEntity.userId
