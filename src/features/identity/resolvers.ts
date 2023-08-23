f3487f73b16363b57cb6f2e7cf6dae986e9f11f9
Bobby Lat <bobby.lat@makerx.com.au>
2023-08-23 17:12:11 +0800

    use config settings to get the tenant name for the issuer

--- src/features/identity/resolvers.ts
+++ src/features/identity/resolvers.ts
@@ -1,6 +1,7 @@
 import { dispatch, query } from '../../cqrs/dispatcher'
 import type { Resolvers } from '../../generated/graphql'
 import { CreateOrUpdateIdentityCommand } from './commands/create-or-update-identity'
+import { resolveIssuer } from './issuer-resolver'
 import { FindIdentitiesQuery } from './queries/find-identities-query'
 import { FindTenantIdentitiesQuery } from './queries/find-tenant-identities-query'
 
@@ -12,6 +13,9 @@ export const resolvers: Resolvers = {
     identity: (_, { id }, { dataLoaders: { identities } }) => identities.load(id),
     findIdentities: (_, { where, offset, limit }, context) => query(context, FindIdentitiesQuery, where, offset, limit),
     findTenantIdentities: (_, { where, limit }, context) => query(context, FindTenantIdentitiesQuery, where, limit),
+  },
+  Identity: {
+    issuer: resolveIssuer,
   },
   Issuance: {
     identity: ({ identityId }, _, { dataLoaders: { identities } }) => identities.load(identityId),
