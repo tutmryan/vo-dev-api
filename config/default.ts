8386bfec19f612eae94424e24f47535a1483a1a0
Sam Curry <sam.curry@makerx.com.au>
2023-08-25 11:02:50 +0800

    add more app OID to name mappings

--- config/default.ts
+++ config/default.ts
@@ -102,6 +102,8 @@ const config: DeepPartial<Config> = {
   platformConsumerApps: {
     '5d988fea-e182-4527-bd3a-a4f743121b33': { name: 'Onboarding Demo API (localdev)' },
     'dc8366b4-ba83-48e5-8ab2-9a852a4500c6': { name: 'Onboarding Demo API' },
+    '682447f2-12b8-4ec1-a78e-fef3bf5e42f5': { name: 'Limited Access Client' },
+    'a774bb59-1fb3-47c7-bbe3-d666fe3f6ca8': { name: 'Barhead Demo' },
   },
   identityIssuers: {
     manual: { name: 'Manually Issued' },
