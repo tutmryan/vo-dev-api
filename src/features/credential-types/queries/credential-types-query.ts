881f501ed4911d13bed7d100908976284077c096
Bobby Lat <bobby.lat@makerx.com.au>
2023-09-08 15:36:43 +0800

    add credential types of query to credential types query

--- src/features/credential-types/queries/credential-types-query.ts
+++ src/features/credential-types/queries/credential-types-query.ts
@@ -3,15 +3,19 @@ import { dataSource } from '../../../data'
 
 export async function CredentialTypesQuery(this: QueryContext) {
   const data = await dataSource.query(`
-    select distinct value from (
-      select value
-      from contract
-      cross apply openjson(credential_types_json, '$')
-      union
-      select value
-      from template
-      cross apply openjson(credential_types_json, '$')
-    ) as both order by value
-  `)
+select distinct value from (
+  select value
+  from contract
+  cross apply openjson(credential_types_json, '$')
+  union
+  select value
+  from template
+  cross apply openjson(credential_types_json, '$')
+  union
+  select value
+  from partner
+  cross apply openjson(credential_types_json, '$')
+) as all_types order by value
+`)
   return data.map(({ value }: { value: string }) => value)
 }
