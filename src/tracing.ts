b3c189c57738c26ee8903f559c5734c8a7ff062f
Sam Curry <cuzzlor@users.noreply.github.com>
2023-10-05 13:41:11 +0800

    add @jenniferplusplus/opentelemetry-instrumentation-bullmq (#132)

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,15 +1,16 @@
+import { BullMQInstrumentation } from '@jenniferplusplus/opentelemetry-instrumentation-bullmq'
+import type { ProxyTracerProvider } from '@opentelemetry/api'
+import { trace } from '@opentelemetry/api'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express'
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql'
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis'
 import { TediousInstrumentation } from '@opentelemetry/instrumentation-tedious'
 import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston'
 import { Resource } from '@opentelemetry/resources'
+import type { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { useAzureMonitor } from 'applicationinsights'
-import type { ProxyTracerProvider } from '@opentelemetry/api'
-import { trace } from '@opentelemetry/api'
-import type { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
-import { registerInstrumentations } from '@opentelemetry/instrumentation'
 
 const resource = Resource.EMPTY
 resource.attributes[SemanticResourceAttributes.SERVICE_NAME] = 'VerifiableOrchestration'
@@ -42,5 +43,6 @@ registerInstrumentations({
     new GraphQLInstrumentation(),
     new TediousInstrumentation(),
     new IORedisInstrumentation(),
+    new BullMQInstrumentation(),
   ],
 })
