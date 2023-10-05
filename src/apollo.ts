298d16f101ac1034fe790dcdbd9121f81bd2cc7b
Sam Curry <cuzzlor@users.noreply.github.com>
2023-10-05 15:51:50 +0800

    Remove apollo caching, re-enable background job processing (#134)
    * remove apollo caching, re-enable background job processing
    * remove @cacheControl directives from schema

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,8 +1,6 @@
-import type { ApolloServerPlugin, GraphQLRequestContext } from '@apollo/server'
+import type { ApolloServerPlugin } from '@apollo/server'
 import { ApolloServer } from '@apollo/server'
-import responseCachePlugin from '@apollo/server-plugin-response-cache'
 import { expressMiddleware } from '@apollo/server/express4'
-import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 import { ApolloServerPluginInlineTrace } from '@apollo/server/plugin/inlineTrace'
 import {
@@ -16,7 +14,7 @@ import { isProduction } from '@makerx/node-common'
 import type { Express } from 'express'
 import { json } from 'express'
 import type http from 'http'
-import { newCacheSection } from './cache'
+import { useBackgroundJob } from './background-jobs'
 import config from './config'
 import type { GraphQLContext } from './context'
 import { createContext, createSubscriptionContext } from './context'
@@ -37,11 +35,6 @@ const plugins = (httpServer: http.Server, serverCleanup?: () => Promise<void>): 
         }
       },
     },
-    ApolloServerPluginCacheControl({ defaultMaxAge: 0 }),
-    responseCachePlugin({
-      cache: newCacheSection('apollo'),
-      sessionId: (requestContext: GraphQLRequestContext<GraphQLContext>) => Promise.resolve(requestContext.contextValue.user?.id ?? null),
-    }),
   ]
   if (!isProduction) {
     plugins.push(ApolloServerPluginInlineTrace())
@@ -69,15 +62,15 @@ export const startApolloServer = async (app: Express, httpServer: http.Server) =
     verifyToken: (host, token) => verifyForHost(host, token, config.get('auth.bearer')),
   })
 
-  // logger.info('Starting background job processing')
-  // const jobRunnerCleanup = useBackgroundJob()
+  logger.info('Starting background job processing')
+  const jobRunnerCleanup = useBackgroundJob()
 
   logger.info('Starting apollo server')
   const server = new ApolloServer<GraphQLContext>({
     logger,
     schema,
     plugins: plugins(httpServer, async () => {
-      await Promise.all([wsServerCleanup.dispose() /*, jobRunnerCleanup.dispose()*/])
+      await Promise.all([wsServerCleanup.dispose(), jobRunnerCleanup.dispose()])
     }),
     introspection: true,
     csrfPrevention: true,
