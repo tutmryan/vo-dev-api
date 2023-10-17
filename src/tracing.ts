f9a2dd0d236b7ca99f6855c6afc40ccac863ec39
Sam Curry <cuzzlor@users.noreply.github.com>
2023-10-17 14:35:25 +0800

    add `logLevel` record prop via winston instrumentation `logHook` (#151)

--- src/tracing.ts
+++ src/tracing.ts
@@ -6,6 +6,7 @@ import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express'
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql'
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis'
 import { TediousInstrumentation } from '@opentelemetry/instrumentation-tedious'
+import type { LogHookFunction } from '@opentelemetry/instrumentation-winston'
 import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston'
 import { Resource } from '@opentelemetry/resources'
 import type { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
@@ -35,11 +36,26 @@ useAzureMonitor({
 
 const tracerProvider = (trace.getTracerProvider() as ProxyTracerProvider).getDelegate() as NodeTracerProvider
 
+/**
+ * AppInsights *seems to* use the `level` property of the record to determine the `SeverityLevel` of the AppInsights log entry.
+ * It appears to translate the npm log levels, e.g. verbose becomes 0; info becomes 1; error becomes 2.
+ * Unfortunately it also seems to remove the `level` property on ingestion, before the log record becomes `customDimensions` in AppInsights `traces` (and `Properties` in Log Analytics `AppTraces`).
+ * I have checked the @opentelemetry/instrumentation-winston source and it does not appear to remove the `level` property or otherwise modify the log record at all.
+ * This behaviour prevents us from using the `level` property to filter logs in App Insights or Log Analytics (e.g. `where Properties.level == 'audit'`).
+ * To work around this, add a `logLevel` property to the record and set it to the same value as the `level` property.
+ * We can then filter logs using the `logLevel` property instead.
+ */
+const winstonAppInsightsLogHook: LogHookFunction = (span, record: Record<string | symbol, any>) => {
+  record['logLevel'] = record.level
+}
+
 registerInstrumentations({
   tracerProvider,
   instrumentations: [
     new ExpressInstrumentation(),
-    new WinstonInstrumentation(),
+    new WinstonInstrumentation({
+      logHook: winstonAppInsightsLogHook,
+    }),
     new GraphQLInstrumentation(),
     new TediousInstrumentation(),
     new IORedisInstrumentation(),
