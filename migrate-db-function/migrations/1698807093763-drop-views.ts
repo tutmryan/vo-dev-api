fc026b2606c49d4f56982d96039518b63844f862
Bobby Lat <bobby.lat@makerx.com.au>
2023-11-01 11:13:15 +0800

    drop views created for power bi reporting which are now out of date

---
+++ migrate-db-function/migrations/1698807093763-drop-views.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class DropViews1698807093763 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DROP VIEW contract_view
+    `)
+    await queryRunner.query(`
+        DROP VIEW issuance_view
+    `)
+    await queryRunner.query(`
+        DROP VIEW presentation_contracts_view
+    `)
+    await queryRunner.query(`
+        DROP VIEW presentation_view
+    `)
+    await queryRunner.query(`
+        DROP VIEW template_view
+    `)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
