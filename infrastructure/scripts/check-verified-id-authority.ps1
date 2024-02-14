830ac937c5a799bcf39bbade625dfba5f473d731
Bobby Lat <bobby.lat@makerx.com.au>
2024-02-14 13:37:49 +0800

    use correct output method

--- infrastructure/scripts/check-verified-id-authority.ps1
+++ infrastructure/scripts/check-verified-id-authority.ps1
@@ -17,32 +17,36 @@ $constants = @{
   didResourceId = '6a8b4b39-c021-437c-b060-5a14a3fd65f3'
 }
 
+$authorityExists = $false
+$linkedDomainsVerified = $false
+
 if ('' -ne $HomeTenantAuthorityId) {
-  return @{
-    authorityExists       = $true
-    linkedDomainsVerified = $true
+  $authorityExists = $true
+  $linkedDomainsVerified = $true
+
+} else {
+
+  #
+  # Check if the Verified ID service is already enabled
+  #
+
+  $authorities = az rest `
+    --url https://verifiedid.did.msidentity.com/v1.0/verifiableCredentials/authorities `
+    --resource $constants.didResourceId `
+    --query 'value' | ConvertFrom-Json
+
+  $authority = $authorities | Where-Object -FilterScript { $_.didModel.linkedDomainUrls -eq $LinkedDomainUrl }[0]
+
+  if ($null -ne $authority) {
+    $authorityExists = $true
+    $linkedDomainsVerified = $authority.linkedDomainsVerified
   }
 }
 
-#
-# Check if the Verified ID service is already enabled
-#
-
-$authorities = az rest `
-  --url https://verifiedid.did.msidentity.com/v1.0/verifiableCredentials/authorities `
-  --resource $constants.didResourceId `
-  --query 'value' | ConvertFrom-Json
-
-$authority = $authorities | Where-Object -FilterScript { $_.didModel.linkedDomainUrls -eq $LinkedDomainUrl }[0]
-
-if ($null -ne $authority) {
-  return @{
-    authorityExists       = $true
-    linkedDomainsVerified = $authority.linkedDomainsVerified
-  }
-}
+Write-Output "authorityExists=$($authorityExists)" >> $Env:GITHUB_OUTPUT
+Write-Output "linkedDomainsVerified=$($linkedDomainsVerified)" >> $Env:GITHUB_OUTPUT
 
 return @{
-  authorityExists       = $false
-  linkedDomainsVerified = $false
+  authorityExists       = $authorityExists
+  linkedDomainsVerified = $linkedDomainsVerified
 }
