2c2051228424f478cc4420b76425fb5ae09a4d0a
Bobby Lat <bobby.lat@makerx.com.au>
2024-02-26 15:59:59 +0800

    add production deploy job and pentest instance in matrix

--- config/production.ts
+++ config/production.ts
@@ -1,11 +1,29 @@
 import type { Config } from '../src/config'
 import type { DeepPartial } from '../src/util/type-helpers'
 
-// refer to: https://github.com/VerifiedOrchestration/verified-orchestration-api/issues/214
-
 const config: DeepPartial<Config> = {
-  cors: {
-    origin: [],
+  platformTenant: {
+    tenantId: '3d804793-53dd-40a2-9d8a-9f67a92fd349',
+  },
+  apiClient: {
+    uri: 'api://verified-orchestration',
+    credentials: {
+      clientId: '78a8e778-b379-4b99-b236-90c920f3a83f',
+    },
+  },
+  internalClient: {
+    uri: 'api://verified-orchestration-internal',
+    credentials: {
+      clientId: '527e0466-d47b-41ef-8c0a-db9a317eb77f',
+    },
+  },
+  callbackCredentials: {
+    clientId: 'f437f4fa-25c5-493a-a23c-c92adf42e082',
+  },
+  limitedAccess: {
+    credentials: {
+      clientId: '137d81ce-2067-4670-9c61-70bb7fc726e8',
+    },
   },
 }
 
