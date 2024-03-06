ce034b0e51d03f8977ca79851bc76fc484579d6b
Bobby Lat <bobby.lat@makerx.com.au>
2024-03-06 13:40:42 +0800

    remove try catch

--- infrastructure/scripts/create-partner-admin-link.ps1
+++ infrastructure/scripts/create-partner-admin-link.ps1
@@ -7,37 +7,30 @@ param (
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
-
-try {
-  # Check required Az.ManagementPartner is installed
-  if (Get-InstalledModule -Name 'Az.ManagementPartner' -ErrorAction SilentlyContinue) {
-    # Module installed
-  } else {
-    Set-PSRepository -Name "PSGallery" -SourceLocation 'https://www.powershellgallery.com/api/v2' -InstallationPolicy Trusted
-    Install-Module Az.ManagementPartner
-  }
-
-  # Collect Azure Partner Id
-  $existingAzurePALIdInfo = $null
-  $existingAzurePALIdInfo = Get-AzManagementPartner -ErrorAction SilentlyContinue
-
-  if ($existingAzurePALIdInfo) {
-    Write-Host "Azure Partner Id already set to:" $existingAzurePALIdInfo.PartnerId
-    Write-Host "The exisiting Azure Partner name is:" $existingAzurePALIdInfo.PartnerName
-  } else {
-    Write-Host "Azure PAL not set on this account in this tenancy."
-  }
-
-  # Check if Azure Azure Partner Ids are the same. If not, set it.
-  if (($null -ne $existingAzurePALIdInfo) -and ($existingAzurePALIdInfo.PartnerId -eq $PartnerId.Tostring())) {
-    Write-Host "✅  Azure PAL Id is alredy set to ${$PartnerId}. Exiting..."
-    break
-  } else {
-    Write-Host "Setting Azure Partner Admin Link to:" $PartnerId
-    New-AzManagementPartner -PartnerId $PartnerId
-  }
-} catch {
-  Write-Exception $_
-  throw
+# Check required Az.ManagementPartner is installed
+if (Get-InstalledModule -Name 'Az.ManagementPartner' -ErrorAction SilentlyContinue) {
+  # Module installed
+} else {
+  Set-PSRepository -Name "PSGallery" -SourceLocation 'https://www.powershellgallery.com/api/v2' -InstallationPolicy Trusted
+  Install-Module Az.ManagementPartner
 }
 
+# Collect Azure Partner Id
+$existingAzurePALIdInfo = $null
+$existingAzurePALIdInfo = Get-AzManagementPartner -ErrorAction SilentlyContinue
+
+if ($existingAzurePALIdInfo) {
+  Write-Host "Azure Partner Id already set to:" $existingAzurePALIdInfo.PartnerId
+  Write-Host "The exisiting Azure Partner name is:" $existingAzurePALIdInfo.PartnerName
+} else {
+  Write-Host "Azure PAL not set on this account in this tenancy."
+}
+
+# Check if Azure Azure Partner Ids are the same. If not, set it.
+if (($null -ne $existingAzurePALIdInfo) -and ($existingAzurePALIdInfo.PartnerId -eq $PartnerId.Tostring())) {
+  Write-Host "✅  Azure PAL Id is alredy set to ${$PartnerId}. Exiting..."
+  break
+} else {
+  Write-Host "Setting Azure Partner Admin Link to:" $PartnerId
+  New-AzManagementPartner -PartnerId $PartnerId
+}
