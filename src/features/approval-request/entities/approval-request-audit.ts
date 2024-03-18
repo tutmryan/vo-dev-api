aa6a3272ff2bcd4310c192ff4ddfc6391d593b6a
Bobby Lat <bobby.lat@makerx.com.au>
2024-03-18 17:42:21 +0800

    fix lint issue

--- src/features/approval-request/entities/approval-request-audit.ts
+++ src/features/approval-request/entities/approval-request-audit.ts
@@ -2,4 +2,4 @@ import { Entity } from 'typeorm'
 import { AuditBase } from '../../auditing/entities/audit-base'
 
 @Entity('approval_request_audit')
-export abstract class ApprovalAudit extends AuditBase { }
+export abstract class ApprovalAudit extends AuditBase {}
