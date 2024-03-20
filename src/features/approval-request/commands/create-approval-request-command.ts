a0fb7ca1b1a56b6fab619943f375339f1daf1ae3
Bobby Lat <bobby.lat@makerx.com.au>
2024-03-20 15:01:00 +0800

    add return type def to command

--- src/features/approval-request/commands/create-approval-request-command.ts
+++ src/features/approval-request/commands/create-approval-request-command.ts
@@ -1,10 +1,10 @@
 import { portalUrl } from '../../../config'
 import type { CommandContext } from '../../../cqs'
-import type { ApprovalRequestInput } from '../../../generated/graphql'
+import type { ApprovalRequestInput, ApprovalRequestResponse } from '../../../generated/graphql'
 import { userInvariant } from '../../../util/user-invariant'
 import { ApprovalRequestEntity } from '../entities/approval-request-entity'
 
-export async function CreateApprovalRequestCommand(this: CommandContext, input: ApprovalRequestInput) {
+export async function CreateApprovalRequestCommand(this: CommandContext, input: ApprovalRequestInput): Promise<ApprovalRequestResponse> {
   const { user, entityManager } = this
 
   userInvariant(user)
