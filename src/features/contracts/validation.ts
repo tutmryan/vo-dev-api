654251cfd4a9ec21030871a7151d33ec6ec6b336
Tu Tran <tu@wearebloom.com.au>
2024-10-02 16:27:21 +0700

    DEV-674 Add validate contract credential types input to prevent VerifiedEmployee being created

--- src/features/contracts/validation.ts
+++ src/features/contracts/validation.ts
@@ -2,13 +2,17 @@ import type { ContractInput, TemplateInput } from '../../generated/graphql'
 import { parseDataUrl } from '../../util/data-url'
 import { validateContractClaims } from './claims'
 
+const notSupportedCredentialTypes = ['VerifiedEmployee']
+
 export function validateContractInput(input: ContractInput) {
   validateContractClaims(input.display.claims)
   validateDisplayLogoImage(input.display.card.logo.image)
+  validateContractCredentialTypes(input.credentialTypes)
 }
 
 export function validateTemplateInput(input: TemplateInput) {
   validateContractClaims(input.display?.claims)
+  if (input.credentialTypes) validateContractCredentialTypes(input?.credentialTypes)
   if (input.display?.card?.logo?.image) validateDisplayLogoImage(input.display.card.logo.image)
 }
 
@@ -17,5 +21,14 @@ function validateDisplayLogoImage(displayLogoImage: string) {
     parseDataUrl(displayLogoImage, { validMimeTypes: ['image/png', 'image/jpeg'], validEncodings: ['base64'] })
   } catch (error) {
     throw new Error('Logo image must be a valid image/png or image/jpeg data URL with base64 encoding')
+  }
+}
+
+export function validateContractCredentialTypes(credentialTypes: ContractInput['credentialTypes']) {
+  if (!credentialTypes) return
+
+  const notSupportedCredentialIndex = credentialTypes.findIndex((type) => notSupportedCredentialTypes.includes(type))
+  if (notSupportedCredentialIndex !== -1) {
+    throw new Error(`${credentialTypes[notSupportedCredentialIndex]} is not a supported credential type`)
   }
 }
