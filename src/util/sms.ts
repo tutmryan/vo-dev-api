1fbc9333b9ed55c49f263b5a64123a17a3c7a821
Wade Baglin <wade.baglin@makerx.com.au>
2024-10-16 15:18:44 +1000

    Adds support for test email and phone numbers

--- src/util/sms.ts
+++ src/util/sms.ts
@@ -4,6 +4,29 @@ import { localDev, sms } from '../config'
 import { logger } from '../logger'
 import { Lazy } from './lazy'
 
+// 🚨🚨 Do not update this without updating the docs-site documentation and schema.graphql code docs 🚨🚨
+// Set uses a hash table internally, so it's O(1) for lookups and not O(n) like an array
+const RESERVED_MOBILES_FOR_TESTING = new Set([
+  // AU https://www.acma.gov.au/phone-numbers-use-tv-shows-films-and-creative-works
+  '+61491570006',
+  '+61491570157',
+  '+61491570737',
+  '+61491573087',
+  '+61491578957',
+  // US https://www.nationalnanpa.com/pdf/NRUF/ATIS-0300115.pdf
+  '+15005550119',
+  '+15005550129',
+  '+15005550139',
+  '+15005550149',
+  '+15005550159',
+  // UK https://www.ofcom.org.uk/phones-and-broadband/phone-numbers/numbers-for-drama
+  '+447700900018',
+  '+447700900028',
+  '+447700900038',
+  '+447700900048',
+  '+447700900058',
+])
+
 const client = Lazy(() => {
   return twilio(sms.sid, sms.secret, { accountSid: sms.accountSid })
 })
@@ -25,6 +48,12 @@ export function sendSms(to: string, message: string) {
       return
     }
   }
+
+  if (RESERVED_MOBILES_FOR_TESTING.has(to)) {
+    logger.warn(`Blocked sending sms to ${maskPhone(to)} as it is a reserved number for testing`)
+    return
+  }
+
   return client().messages.create({
     body: message,
     from: sms.from,
