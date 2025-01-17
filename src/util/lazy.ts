e26419c981391aa572e7181e55cf729ce5505f64
Sam Curry <cuzzlor@users.noreply.github.com>
2025-01-17 14:29:44 +0800

    When `Lazy` promises reject, evict them from the cached result (#642)

--- src/util/lazy.ts
+++ src/util/lazy.ts
@@ -1,3 +1,5 @@
+import { logger } from '../logger'
+
 const NoValue = Symbol('no-value')
 
 type LazyInstance<T> = () => T
@@ -7,6 +9,13 @@ export const Lazy = <T>(factory: () => T): LazyInstance<T> => {
   return () => {
     if (val === NoValue) {
       val = factory()
+    }
+    // evict promise values when they reject
+    if (val instanceof Promise) {
+      val.catch((error) => {
+        logger.warn('Lazy promise rejected, evicting result', { error })
+        val = NoValue
+      })
     }
     return val
   }
