d8f3414cd407a97de5254e9942b07fde4fdae56d
Wade Baglin <wade.baglin@makerx.com.au>
2025-02-06 17:20:39 +1000

    chore: check for usage before deleting a template

--- src/features/templates/commands/delete-template-command.ts
+++ src/features/templates/commands/delete-template-command.ts
@@ -1,8 +1,15 @@
 import { basename } from 'path'
 import type { CommandContext } from '../../../cqs'
+import { invariant } from '../../../util/invariant'
+import { ContractEntity } from '../../contracts/entities/contract-entity'
 import { TemplateEntity } from '../entities/template-entity'
 
 export async function DeleteTemplateCommand(this: CommandContext, id: string) {
+  const usedByContracts = await this.entityManager.getRepository(ContractEntity).findBy({
+    templateId: id,
+  })
+  invariant(usedByContracts.length === 0, 'Cannot delete template which used by one or more contract.')
+
   const repo = this.entityManager.getRepository(TemplateEntity)
   const template = await repo.findOneByOrFail({ id })
   await repo.remove(template)
