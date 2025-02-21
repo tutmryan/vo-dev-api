1578e5b63a0f7232b4d913b03dd3119a5fb817dd
Wade Baglin <wade.baglin@makerx.com.au>
2025-02-21 16:06:49 +1000

    Revert "chore: deployment test (stop before swap to minimise app recycle)"
    This reverts commit d9173912f5d7f0c4f154daf613e066dfb01229b5.

--- infrastructure/scripts/swap-staging-slot.ps1
+++ infrastructure/scripts/swap-staging-slot.ps1
@@ -35,13 +35,6 @@ if ($slotState -ne "Running") {
     --slot $SourceSlotName
 }
 
-Write-Host "Stopping slot '$SourceSlotName' for '$AppServiceName'"
-
-az webapp stop `
-  --resource-group $ResourceGroupName `
-  --name $AppServiceName `
-  --slot $SourceSlotName
-
 Write-Host "Swapping slot '$SourceSlotName' to '$TargetSlotName' for '$AppServiceName'"
 
 az webapp deployment slot swap `
@@ -49,3 +42,10 @@ az webapp deployment slot swap `
   --name $AppServiceName `
   --slot $SourceSlotName `
   --target-slot $TargetSlotName
+
+Write-Host "Stopping slot '$SourceSlotName' for '$AppServiceName'"
+
+az webapp stop `
+  --resource-group $ResourceGroupName `
+  --name $AppServiceName `
+  --slot $SourceSlotName
