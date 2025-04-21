cad7c5c839cf83f8c1b7e0c0924d0b89f0e128b6
Wade Baglin <wade.baglin@makerx.com.au>
2025-04-21 19:26:58 +1000

    chore(test): add faker for data, and give a reason for embedded

--- src/test/data-generators.ts
+++ src/test/data-generators.ts
@@ -4,16 +4,26 @@ import examples from 'libphonenumber-js/examples.mobile.json'
 import type { CountryCode } from 'libphonenumber-js/types'
 import { throwError } from '../util/throw-error'
 
-export function randomElement<T>(elements: T[]): T {
+function randomElement<T>(elements: T[]): T {
   return elements[randomNumberBetween(0, elements.length - 1)] as T
 }
 
-export function randomNumberBetween(min: number, max: number): number {
+function randomNumberBetween(min: number, max: number): number {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+/**
+ * Generates a random phone number in E.164 compliant format.
+ *
+ * - This function randomly selects a country code from the available libphonenumber-js examples and generates a random phone number for that country.
+ * - The generated phone number is formatted in E.164 format.
+ *
+ * @returns {string} A random phone number in E.164 format.
+ *
+ * @throws {Error} If the random phone number generation fails.
+ */
 export function randomPhoneNumber(): string {
   const countryPhoneCodes = Object.keys(examples as Examples) as CountryCode[]
   const randomCountryPhoneCode = randomElement(countryPhoneCodes)
