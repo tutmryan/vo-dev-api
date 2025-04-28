816a6f0896b4ffbfc0d488d48177bab59c6269a9
Sam Curry <sam.curry@makerx.com.au>
2025-04-28 18:37:26 +0800

    DEV-1013 fix: do not report services as unavailable before checking state

--- src/services/monitoring/express.ts
+++ src/services/monitoring/express.ts
@@ -7,10 +7,10 @@ export async function addServiceHealthEndpoints(app: Express): Promise<void> {
   // add service health endpoints
   Object.keys(serviceState).forEach((service) => {
     app.get(`/health/services/${service}`, (req, res) => {
-      if (serviceState[service as keyof ServiceState]) {
-        res.status(200).send('OK').end()
-      } else {
+      if (serviceState[service as keyof ServiceState] === false) {
         res.status(503).send('Not OK').end()
+      } else {
+        res.status(200).send('OK').end()
       }
     })
     logger.info(`Added GET /health/services/${service}`)
