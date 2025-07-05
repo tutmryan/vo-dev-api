3d65b1ee49d86466e06a35aeadef99664efd8d16
Sam Curry <sam.curry@makerx.com.au>
2025-07-05 16:20:19 +0800

    fix: delete sql database script to handle the database not existing

--- infrastructure/scripts/delete-sql-database.ps1
+++ infrastructure/scripts/delete-sql-database.ps1
@@ -19,24 +19,28 @@ $PSNativeCommandUseErrorActionPreference = $true
 # Check if the database exists
 Write-Output "Checking if database '$DatabaseName' exists on server '$SqlServerName' in resource group '$ResourceGroupName'..."
 
-$database = az sql db show `
-  --resource-group $ResourceGroupName `
-  --server $SqlServerName `
-  --name $DatabaseName `
-  --query 'name' `
-  --output tsv 2>$null
-
-if ($database -eq $DatabaseName) {
-  Write-Output "Deleting SQL database '$DatabaseName' from server '$SqlServerName' in resource group '$ResourceGroupName'..."
-
-  # Delete the database
-  az sql db delete `
+try {
+  $database = az sql db show `
     --resource-group $ResourceGroupName `
     --server $SqlServerName `
     --name $DatabaseName `
-    --yes
+    --query 'name' `
+    --output tsv 2>$null
 
-  Write-Output "Deleted SQL database '$DatabaseName' ✅"
-} else {
+  if ($database -eq $DatabaseName) {
+    Write-Output "Deleting SQL database '$DatabaseName' from server '$SqlServerName' in resource group '$ResourceGroupName'..."
+
+    # Delete the database
+    az sql db delete `
+      --resource-group $ResourceGroupName `
+      --server $SqlServerName `
+      --name $DatabaseName `
+      --yes
+
+    Write-Output "Deleted SQL database '$DatabaseName' ✅"
+  } else {
+    Write-Output "SQL database '$DatabaseName' does not exist on server '$SqlServerName' ❌"
+  }
+} catch {
   Write-Output "SQL database '$DatabaseName' does not exist on server '$SqlServerName' ❌"
 }
